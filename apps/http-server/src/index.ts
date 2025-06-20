792f05789258387589f28c38effe005eeaca15ba
Tusharkumar888 <tushakumar888219@gmail.com>
2025-06-21 01:19:34 +0530

    init

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,9 +3,19 @@ import { Request, Response } from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req:Request, res:Response) => {
+app.post('/signup', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
+
+
+app.post('/signin',(req:Request, res:Response)=>{
+
+})
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
